454d028f53e49758a0872e96ceaba100386cc364
Bi <100726683+biancaraetchi@users.noreply.github.com>
2023-03-30 13:40:18 +0200

    Adding class for excel reading (#4)
    * Added some abstraction to graphs for graph_generator
    
    * Added ExcelReader

--- app/graph_generator/data/excel_reader.py
+++ app/graph_generator/data/excel_reader.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
+class ExcelReader:
+    def read_file(file):
+        return pd.read_csv(file, header=0, names=0)
 
 def player_data(player_file):
     """
     Returns a dataframe representing the passed player data excel file
     """
-
-    data = pd.read_csv(player_file, header=0, names=0)
+    data = ExcelReader().read_file(player_file)
     return data
 
 
@@ -14,8 +16,7 @@ def league_data(league_file, player_name):
     """
     Returns a dataframe representing the league data of a passed player
     """
-
-    dataframe = pd.read_csv(league_file, header=0, names=0)
+    dataframe = ExcelReader().read_file(league_file)
     try:
         player = dataframe.loc[dataframe['Player'] == player_name]
     except KeyError:
