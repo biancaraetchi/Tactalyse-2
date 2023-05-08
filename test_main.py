083c04265d3a741a0dde086e5fb1e2e1df6455d2
Bi <100726683+biancaraetchi@users.noreply.github.com>
2023-05-08 15:45:15 +0200

    Scatter plot (#13)
    * Added dynamic scatterplots
    
    * Update test_main.py
    
    * Added changes to test_main
    
    ---------
    
    Co-authored-by: Bianca Raetchi <bianca.raetchi@gmail.com>

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 from app.controller.graph_service import *
-from app.controller.data_service import get_bar_data, get_line_data, get_radar_data,get_pdf_data
+from app.controller.data_service import get_bar_data, get_line_data, get_radar_data,get_pdf_data, get_scatter_data
 from app.controller.pdf_service import create_pdf
 import pandas as pd
 import os
@@ -18,20 +18,17 @@ def generate_pdf():
     end_date = None
 
     radar_map = get_radar_data(league_file, player_name, compare_name)
-    line_map = get_line_data(league_file, player_file, player_name, compare_file, compare_name, start_date, end_date)
+    line_map = get_line_data(league_file, player_file, player_name, compare_file, compare_name, start_date, end_date) 
     bar_map_base = get_bar_data(league_file, player_name, compare_name)
     bar_map_main_stats = get_bar_data(league_file, player_name, 'I. Sarr')
+    scatter_map=get_scatter_data(player_file)
 
     # Pass the maps to get lists containing plots in byte form from the graph_generator module
     radar_chart = create_radar_chart(radar_map)
     line_plots = create_line_plots(line_map)
     bar_plots = create_bar_plots(bar_map_base, 'v')
     main_stats_bar_plot = create_main_stats_bar_plot(bar_map_main_stats)
-
-    wtv = {'Interceptions': [1, 2, 4, 2, 6, 3], 'Passes': [3, 4, 2, 7, 4, 2]}
-    wtvdf = pd.DataFrame(data=wtv)
-    scatter_plot = create_scatter_plot(wtvdf)
-    scatter_plots = [scatter_plot, scatter_plot]
+    scatter_plots=create_scatter_plots(scatter_map)
 
     # Get a parameter map with relevant data for generating a PDF from the data module, and pass it to the pdf_generator
     # module along with the graphs
