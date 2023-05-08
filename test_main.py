0123ceb63609cd1ea025f11814a2f419ee3be58a
Bianca Raetchi <bianca.raetchi@gmail.com>
2023-05-08 15:35:23 +0200

    Added changes to test_main

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 from app.controller.graph_service import *
-from app.controller.data_service import get_bar_data, get_line_data, get_radar_data,get_pdf_data
+from app.controller.data_service import get_bar_data, get_line_data, get_radar_data,get_pdf_data, get_scatter_data
 from app.controller.pdf_service import create_pdf
 import pandas as pd
 import os
@@ -20,17 +20,19 @@ def generate_pdf():
     radar_map = get_radar_data(league_file, player_name, compare_name)
     line_map = get_line_data(league_file, player_file, player_name, compare_file, compare_name, start_date, end_date)
     bar_map = get_bar_data(league_file, player_name)
+    scatter_map = get_scatter_data(player_file)
 
     # Pass the maps to get lists containing plots in byte form from the graph_generator module
     radar_chart = create_radar_chart(radar_map)
     line_plots = create_line_plots(line_map)
     bar_plots = create_bar_plots(bar_map, 'v')
     main_stats_bar_plot = create_main_stats_bar_plot(bar_map)
+    scatter_plots=create_scatter_plots(scatter_map)
 
-    wtv = {'Interceptions': [1, 2, 4, 2, 6, 3], 'Passes': [3, 4, 2, 7, 4, 2]}
-    wtvdf = pd.DataFrame(data=wtv)
-    scatter_plot = create_scatter_plot(wtvdf)
-    scatter_plots = [scatter_plot, scatter_plot]
+    # wtv = {'Interceptions': [1, 2, 4, 2, 6, 3], 'Passes': [3, 4, 2, 7, 4, 2]}
+    # wtvdf = pd.DataFrame(data=wtv)
+    # scatter_plot = create_scatter_plot(wtvdf)
+    # scatter_plots = [scatter_plot, scatter_plot]
 
     # Get a parameter map with relevant data for generating a PDF from the data module, and pass it to the pdf_generator
     # module along with the graphs
