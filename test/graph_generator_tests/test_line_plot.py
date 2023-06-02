a43e4fdfa790060d99c22a5385f489420528822b
MikkoBra <m.brandon.1@student.rug.nl>
2023-06-02 17:29:22 +0200

    Test draw line plot returns png bytes

--- test/graph_generator_tests/test_line_plot.py
+++ test/graph_generator_tests/test_line_plot.py
@@ -1,13 +1,29 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from app.graph_generator.graphs.line_plot import LinePlot
 from app.graph_generator.graphs.line_plot_data_helper import LinePlotDataHelper
+import pandas as pd
 
 
 class TestLinePlot(unittest.TestCase):
 
     def setUp(self):
         self.plot = LinePlot({'player_pos': 'pos'})
+        dates = ['2020/01/01', '2020/01/02', '2020/01/03', '2020/01/04', '2020/01/05', '2020/01/06', '2020/01/07']
+        data_p1 = [20, 3, 49, 3, 5, 6, 1]
+        player_data = pd.DataFrame({'Date': dates, 'Stat': data_p1})
+        data_p2 = [20, 5, 29, 13, 3, 15, 3]
+        compare_data = pd.DataFrame({'Date': dates, 'Stat': data_p2})
+
+        self.params = {
+            "player_data": player_data,
+            "compare_data": compare_data,
+            "columns": "Stat",
+            "start_date": "2020/01/02",
+            "end_date": "2020/01/06",
+            "player": "J. Doe",
+            "compare": "D. Man"
+        }
 
     def test_constructor(self):
         self.assertIsInstance(self.plot.helper, LinePlotDataHelper)
@@ -32,6 +48,12 @@ class TestLinePlot(unittest.TestCase):
             result = self.plot.draw_all(params)
             expected = 'plot'
             self.assertEqual(expected, result)
+
+    def test_draw_returns_png(self):
+        plot = self.plot.draw(self.params)
+        self.assertNotEqual(plot, None, 'no changes')
+        self.assertTrue(plot.startswith(b'\x89PNG'), 'Wrong graph format. Expected PNG.')
+
 
 if __name__ == "__main__":
     unittest.main()
